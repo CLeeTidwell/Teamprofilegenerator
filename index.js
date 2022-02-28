06792b71cd993328a8424c2398da1002ea11fcfd
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-02-27 22:26:32 -0800

    write index.js and employee.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/* require inquirer, fs and teamtemplate.JS */
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeam = require("./teamtemplate/teamtemplate.js");
+
+        /* library modules */
+
+const Engineer = require("./library/Engineer");
+const Intern = require("./library/Intern");
+const Manager = require("./library/Manager");
